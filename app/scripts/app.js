466344ebcecb553caf66ce0d11264b849b063ea7
Junerey Casuga <zeresoft@zeresoft-desktop.(none)>
2014-02-03 18:56:33 +0800

    temp layout for conversion

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -11,7 +11,7 @@ angular.module('vskypeApp', [
       .when('/', {
         templateUrl: 'views/main.html',
         controller: 'MainCtrl'
-      })  
+      })
       .otherwise({
         redirectTo: '/'
       });
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -13,12 +13,14 @@ vskype.controller('MainCtrl', function ($scope) {
 vskype.factory('convert', function(){
 	var convertionService = {};
 
-	convertionService.convertTagcoin = function(){
-		console.log('tag');
+	convertionService.convertTagcoin = function(tag){
+		var tagValue = tag * 40;
+		$('#currValue').val(tagValue);
 	};
 
-	convertionService.convertCurrency = function(){
-		console.log('curr');
+	convertionService.convertCurrency = function(curr){
+		var currValue = curr * 40;
+		$('#tagValue').val(currValue);
 	}
 
 	return convertionService;
