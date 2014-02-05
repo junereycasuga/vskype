e316efb73866bc6f573a896708343a0f1b8fa179
Junerey Casuga <zeresoft@zeresoft-desktop.(none)>
2014-02-05 20:17:46 +0800

    try using bitstamp instead of mtgox

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -18,7 +18,7 @@ vskype.factory('getData', function($http){
 	};
 
 	getDataService.getUSDValOfBTC = function(callback){
-		$http.get('http://data.mtgox.com/api/2/BTCUSD/money/ticker_fast').success(callback);
+		$http.get('https://www.bitstamp.net/api/ticker/').success(callback);
 	};
 
 	return getDataService;
@@ -30,7 +30,7 @@ vskype.factory('convert', function($http, getData){
 	convertionService.getBTCValOfTagCallback = function(res){
 		var TAG_BTC = res['price'];
 		getData.getUSDValOfBTC(function(res2){
-			var BTC_USD = res2['data']['last_all']['value'];
+			var BTC_USD = res2['last'];
 			convertionService.convertTagcoin = function(tag){
 				var TAG_BTC2 = TAG_BTC * tag;
 				var BTC_USD2 = BTC_USD * TAG_BTC2;
