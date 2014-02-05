c68e2850a1ee50f11ee9bb76fa6236ab990727a4
Junerey Casuga <zeresoft@zeresoft-desktop.(none)>
2014-02-05 18:38:54 +0800

    optimized code for converting tag to usd

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -6,65 +6,46 @@ vskype.controller('MainCtrl', function ($scope) {
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
-      'Karma'
+      'Karma',
     ];
 });
 
-vskype.factory('convert', function($http){
+vskype.factory('getData', function($http){
+	var getDataService = {};
+
+	getDataService.getBTCValOfTag = function(callback){
+		$http.get('http://www.cryptocoincharts.info/v2/api/tradingPair/TAG_BTC').success(callback);
+	};
+
+	getDataService.getUSDValOfBTC = function(callback){
+		$http.get('http://data.mtgox.com/api/2/BTCUSD/money/ticker_fast').success(callback);
+	};
+
+	return getDataService;
+});
+
+vskype.factory('convert', function($http, getData){
 	var convertionService = {};
 
-	convertionService.getBTCValOfTag = function(tag){
-		$http({
-			method: 'GET',
-			url: 'http://www.cryptocoincharts.info/v2/api/tradingPair/TAG_BTC'
-		})
-			.success(function(res){
-				var TAG_BTC = res['price'];
-			});
+	convertionService.getBTCValOfTagCallback = function(res){
+		var TAG_BTC = res['price'];
+		getData.getUSDValOfBTC(function(res2){
+			var BTC_USD = res2['data']['last_all']['value'];
+			convertionService.convertTagcoin = function(tag){
+				var TAG_BTC2 = TAG_BTC * tag;
+				var BTC_USD2 = BTC_USD * TAG_BTC2;
+				if(isNaN(BTC_USD)){
+					$('#currValue').val('');
+				} else {
+					$('#currValue').val(Number(BTC_USD2).toFixed(2));
+				}
+			}
+		});
 	};
 
-	convertionService.convertBTCToUSD = function(btc){
-		$http({
-			method: 'GET',
-			url: 'http://data.mtgox.com/api/2/BTCUSD/money/ticker_fast'
-		})
-			.success(function(res){
-				var BTC_USD = res['data']['last_all']['value'] * btc;
-			});
-	};
-
-	convertionService.convertTagcoin = function(tag){
-		$http({
-			method: 'GET',
-			url: 'http://www.cryptocoincharts.info/v2/api/tradingPair/TAG_BTC'
-		})
-			.success(function(res){
-				var TAG_BTC = res['price'] * tag;
-				console.log('1 tag = ' + res['price']);
-				console.log(tag + 'tags = ' + TAG_BTC);
-				$http({
-					method: 'GET',
-					url: 'http://data.mtgox.com/api/2/BTCUSD/money/ticker_fast'
-				})
-					.success(function(res){
-						var BTC_USD = res['data']['last_all']['value'] * TAG_BTC;
-						console.log(res['data']['last_all']['value']);
-						console.log(BTC_USD);
-						if(isNaN(BTC_USD)){
-							$('#currValue').val('');
-						} else {
-							$('#currValue').val(Number(BTC_USD).toFixed(8));
-						}
-					})
-					.error(function(){
-						console.log('can\'t fetch data');
-					});
-			})
-			.error(function(){
-				console.log('can\'t fetch data');
-			});
-		
-	};
+	setInterval(function(){
+		getData.getBTCValOfTag(convertionService.getBTCValOfTagCallback);
+	}, 10000);
 
 	convertionService.convertCurrency = function(curr){
 		var currValue = curr * convertionService.usdVal;
@@ -73,7 +54,7 @@ vskype.factory('convert', function($http){
 		} else {
 			$('#tagValue').val(Number(currValue).toFixed(8));
 		}
-	}
+	};
 
 	return convertionService;
 });
